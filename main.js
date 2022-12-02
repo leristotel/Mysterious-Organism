623f8fea49fe765283a26ac843e81d2fdc192469
Валерия <valerinavinci@MacBook-Pro-Valeria.local>
2022-12-02 14:27:18 +0100

    Add file main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ['A', 'T', 'C', 'G'];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+const pAequorFactory = (number, array) => {
+  return {
+    specimenNum: number,
+    dna: array
+  }
+}
+console.log(pAequorFactory(1,  mockUpStrand()));
+
+
+
+
+
+
+
+
+
